ad99e77c492b12ed32a22ef6aedab720329782de
Tommy Volk <tvolk131@gmail.com>
2022-11-16 12:46:16 -0600

    Frontend can subscribe to invoice payment events

--- client/src/api.ts
+++ client/src/api.ts
@@ -6,8 +6,16 @@ declare global {
   }
 }
 
+import {listen} from '@tauri-apps/api/event';
+
 const {invoke} = window.__TAURI__.tauri;
 
 export async function getInvoice(): Promise<string> {
   return await invoke('get_invoice');
 }
+
+export async function onInvoicePaid(callback: (invoice: string) => void) {
+  return await listen('on_invoice_paid', (event) => {
+    callback(event.payload as string);
+  });
+};
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,25 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
+// TODO - Remove this once we can connect to a lightning node.
+const INVOICE: &str = "lightning:lnbc10u1p3hgmlfpp554ufwa2uaa69uz27t9u6hr8yrskju7mxeqksjdmldpa20rh60lnqdqqcqzpgxqr23ssp55u85dejctg2dln8ff94rrtfjxy7xxk4nehzv7v2uetj4kpp2k5vs9qyyssq3zudhexj3x68n4jydplwpyezjxu8au5ydv9zr50l4gccrp7dzw650nve3jgnayc5e0zfu4vzyt2ktvz7tkpmenm9tzk4vtfnvyrzqcqpj53v9k";
+
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            std::thread::spawn(move || {
+                loop {
+                    // TODO - Remove this and actually implement this
+                    // thread loop once we can connect to a lightning node.
+                    std::thread::sleep(std::time::Duration::from_secs(10));
+                    handle.emit_all("on_invoice_paid", INVOICE).unwrap();
+                }
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![get_invoice])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -12,5 +29,7 @@ fn main() {
 
 #[tauri::command]
 async fn get_invoice() -> String {
-    String::from("lightning:lnbc10u1p3hgmlfpp554ufwa2uaa69uz27t9u6hr8yrskju7mxeqksjdmldpa20rh60lnqdqqcqzpgxqr23ssp55u85dejctg2dln8ff94rrtfjxy7xxk4nehzv7v2uetj4kpp2k5vs9qyyssq3zudhexj3x68n4jydplwpyezjxu8au5ydv9zr50l4gccrp7dzw650nve3jgnayc5e0zfu4vzyt2ktvz7tkpmenm9tzk4vtfnvyrzqcqpj53v9k")
+    // TODO - Remove this mock value and actually implement this
+    // function once we can connect to a lightning node.
+    String::from(INVOICE)
 }
