92ed3d50b99f405a4b001e92f76fb608c8836f6b
Tommy Volk <tvolk131@gmail.com>
2022-12-10 11:43:57 -0600

    Add basic screensaver

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react';
+import {useState, useRef, useCallback} from 'react';
 import {ThemeProvider, createTheme, useTheme} from '@mui/material/styles';
 import {blue} from '@mui/material/colors';
 import {LightningNetworkLogo} from './lightningNetworkLogo';
 import {SelectionMenu} from './selectionMenu';
 import {Helmet} from 'react-helmet';
+import {Typography} from '@mui/material';
+
+// Screensaver appears after one minute of inactivity.
+const SCREENSAVER_DELAY_MS = 60000;
 
 const SubApp = () => {
   const theme = useTheme();
+  
+  const [screensaverActive, setScreensaverActive] = useState(true);
+  const screensaverTimeout = useRef<NodeJS.Timeout>();
+
+  const screensaverClicked = useCallback(() => {
+    setScreensaverActive(false);
+    startTimeout();
+  }, []);
+
+  const startTimeout = useCallback(() => {
+    clearTimeout(screensaverTimeout.current);
+    const timeout = setTimeout(() => setScreensaverActive(true), SCREENSAVER_DELAY_MS);
+    screensaverTimeout.current = timeout;
+  }, []);
+
+  const appTouched = useCallback(ev => {
+    if (ev.target.id !== 'screensaver') {
+      startTimeout();
+    }
+  }, []);
 
   return (
     <div>
@@ -16,13 +41,38 @@ const SubApp = () => {
       <Helmet>
           <style>{`body { background-color: ${theme.palette.background.default}; }`}</style>
       </Helmet>
-      <div style={{display: 'flex', flexWrap: 'wrap', height: '100vh'}}>
+      <div
+        style={{display: 'flex', flexWrap: 'wrap', height: '100vh'}}
+        onClick={appTouched}
+      >
         <div style={{width: 'fit-content', margin: 'auto', padding: '20px'}}>
           <LightningNetworkLogo size={200}/>
         </div>
         <div style={{width: 'fit-content', margin: 'auto'}}>
           <SelectionMenu size={330}/>
         </div>
+        {screensaverActive && (
+          <div
+            id='screensaver'
+            style={{
+              position: 'fixed',
+              left: 0,
+              top: 0,
+              width: '100%',
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: theme.palette.background.default + 'E0',
+              fontSize: 32,
+              color: 'white',
+              cursor: 'pointer'
+            }}
+            onClick={screensaverClicked}
+          >
+            <Typography variant={'h3'}>Tap to Continue</Typography>
+          </div>
+        )}
       </div>
     </div>
   );
