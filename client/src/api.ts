db46f9ffabb51f98bafbc61d3b5e5e55c8caae9d
Tommy Volk <tvolk131@gmail.com>
2022-12-24 18:02:21 -0600

    Reorganize server code

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,15 +1,3 @@
-export {};
-
-declare global {
-  interface Window {
-    __TAURI__: any;
-  }
-}
-
-import {listen} from '@tauri-apps/api/event';
-
-const {invoke} = window.__TAURI__.tauri;
-
 export async function getInvoice(): Promise<string> {
   return await invoke('get_invoice');
 }
