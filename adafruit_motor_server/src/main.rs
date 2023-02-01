ddf078c53ab61092bd5b0fcfa58f1bc954bef168
Tommy Volk <tvolk131@gmail.com>
2023-02-01 05:32:45 +0000

    Adafruit motor server uses consistent interface

--- adafruit_motor_server/src/main.rs
+++ adafruit_motor_server/src/main.rs
@@ -6,12 +6,17 @@ use std::sync::Mutex;
 
 mod vend_coil;
 
-#[get("/spinMotor")]
-fn spin_motor_handler(
-    vend_coil: &State<Mutex<vend_coil::VendCoil>>,
-) -> rocket::response::content::Plain<String> {
-    vend_coil.lock().unwrap().rotate().unwrap();
-    rocket::response::content::Plain("Test response".to_string())
+#[get("/commands/stepper0")]
+fn run_command_handler(vend_coil: &State<Mutex<vend_coil::VendCoil>>) -> Result<rocket::response::content::Plain<String>, rocket::response::status::Custom<String>> {
+    match vend_coil.lock().unwrap().rotate() {
+        Ok(_) => Ok(rocket::response::content::Plain("Success!".to_string())),
+        Err(err) => Err(rocket::response::status::Custom(rocket::http::Status::InternalServerError, format!("Failed to execute action: {}", err)))
+    }
+}
+
+#[get("/listCommands")]
+fn list_commands_handler() -> rocket::response::content::Json<String> {
+    rocket::response::content::Json(serde_json::json!(vec!("stepper0")).to_string())
 }
 
 #[rocket::launch]
@@ -25,5 +30,5 @@ async fn rocket() -> _ {
     rocket::build()
         .manage(Mutex::from(vend_coil))
         .configure(rocket_config)
-        .mount("/", routes![spin_motor_handler])
+        .mount("/", routes![run_command_handler, list_commands_handler])
 }
