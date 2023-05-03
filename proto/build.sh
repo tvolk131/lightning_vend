a2084e55a12b864e48cdbed8486737e8b83eb7dd
Tommy Volk <tvolk131@gmail.com>
2023-05-03 05:43:41 +0000

    LND client has full Typescript typings

--- proto/build.sh
+++ proto/build.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 sudo apt-get install -y protobuf-compiler
-protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=. ./proto/lightning_vend/model.proto ./proto/lnd/lnrpc/lightning.proto
+protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=forceLong=string --ts_proto_out=. ./proto/lightning_vend/model.proto ./proto/lnd/lnrpc/lightning.proto
--- server/lnAuth.ts
+++ server/lnAuth.ts
@@ -1,47 +1,39 @@
-import {VerifyMessageRequest, VerifyMessageResponse} from '../proto/lnd/lnrpc/lightning';
 import {
   createMessageTokenWithExpiration,
   messageIsAuthentic
 } from '../shared/lnAuthNodeOnly';
 import {lightning, lnAuthJwtSecret} from './lnd_api';
 import {messageIsExpired, messageIsMalformed} from '../shared/lnAuthBrowserSafe';
+import {VerifyMessageRequest} from '../proto/lnd/lnrpc/lightning';
 
 export const createSignableMessageWithTTL = (expireTimeSeconds: number) => {
   return createMessageTokenWithExpiration(expireTimeSeconds, lnAuthJwtSecret);
 };
 
 export const verifyMessage =
-(message: string, signature: string): Promise<string> => {
+async (message: string, signature: string): Promise<string> => {
   if (messageIsMalformed(message)) {
-    return Promise.reject('Message is malformed.');
+    throw 'Message is malformed.';
   }
 
   if (!messageIsAuthentic(message, lnAuthJwtSecret)) {
-    return Promise.reject('Message is not authentic.');
+    throw 'Message is not authentic.';
   }
 
   if (messageIsExpired(message)) {
-    return Promise.reject('Message has expired.');
+    throw 'Message has expired.';
   }
 
-  return new Promise((resolve, reject) => {
-    return lightning.verifyMessage(
-      VerifyMessageRequest.create({
-        msg: new TextEncoder().encode(message),
-        signature
-      }),
-      (err: any, rawVerifyMessageResponse: any) => {
-        if (err) {
-          return reject(err);
-        }
+  const response = await lightning.VerifyMessage(
+    VerifyMessageRequest.create({
+      msg: new TextEncoder().encode(message),
+      signature
+    })
+  );
 
-        const verifyMessageResponse = VerifyMessageResponse.fromJSON(rawVerifyMessageResponse);
-        if (!verifyMessageResponse.valid) {
-          return reject('Message signature is not valid.');
-        }
+  if (!response.valid) {
+    throw 'Message signature is not valid.';
+  }
 
-        return resolve(verifyMessageResponse.pubkey);
-      }
-    );
-  });
+  return response.pubkey;
 };
