ca6e2c27e248d42e26c1b44389c5c29c9b025038
Tommy Volk <tvolk131@gmail.com>
2023-05-13 19:01:59 +0000

    Add `DeviceSetupCodeManager` class to server

--- shared/constants.ts
+++ shared/constants.ts
@@ -4,3 +4,7 @@ export const learnMorePagePath = '/learn-more';
 
 export const socketIoAdminPath = '/socket.io-admin';
 export const socketIoDevicePath = '/socket.io-device';
+
+// All uppercase alphanumeric characters (except for 0, O, and I, to prevent confusion).
+export const deviceSetupCodeAllowedCharacters = 'ABCDEFGHJKLMNPQRSTUVWXYZ123456789';
+export const deviceSetupCodeLength = 4;
