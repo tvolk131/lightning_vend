d623933a5474bd0d934fdc3b4e6314c6decb6a39
Tommy Volk <tvolk131@gmail.com>
2023-05-30 17:00:09 +0000

    Cleanup landing page

--- client/src/landingPage.tsx
+++ client/src/landingPage.tsx
@@ -1,123 +1,104 @@
 import * as React from 'react';
-import {CSSProperties, ReactNode, useEffect, useState} from 'react';
+import {CSSProperties, ReactNode, useEffect, useRef, useState} from 'react';
+import {adminPagePath, learnMorePagePath} from '../../shared/constants';
 import {animated, useSpring} from '@react-spring/web';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import BoltIcon from '@mui/icons-material/Bolt';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import IconButton from '@mui/material/IconButton';
 import TuneIcon from '@mui/icons-material/Tune';
 import {Tweet} from 'react-twitter-widgets';
 import Typography from '@mui/material/Typography';
-import {adminPagePath} from '../../shared/constants';
 import {useNavigate} from 'react-router-dom';
 import {useTheme} from '@mui/material/styles';
 import {useTrackVisibility} from 'react-intersection-observer-hook';
+
+const totalSlideAnimationTimeMs = 300;
 
 export const LandingPage = () => {
   const theme = useTheme();
   const navigate = useNavigate();
 
-  const titleStyles: CSSProperties = {
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const subtitleStyles: CSSProperties = {
     marginBottom: theme.spacing(4)
   };
 
-  const subtitleStyles: CSSProperties = {
-    marginBottom: theme.spacing(6)
-  };
-
-  const featureItemStyles: CSSProperties = {
-    alignItems: 'center',
-    marginBottom: theme.spacing(4),
-    display: 'inline-flex'
-  };
-
-  const featureIconStyles: CSSProperties = {
-    marginRight: theme.spacing(2)
-  };
-
   return (
-    <Box sx={{
-      height: '100vh',
-      overflow: 'auto',
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
-      scrollSnapType: 'y mandatory'
-    }}>
-      <LandingPageSection>
-        <div style={titleStyles}>
-          <Typography variant='h3'>
-            LightningVend
-          </Typography>
-        </div>
+    <Box
+      sx={{
+        height: '100vh',
+        overflow: 'auto',
+        textAlign: 'center',
+        alignItems: 'center',
+        justifyContent: 'center',
+        scrollSnapType: 'y mandatory'
+      }}
+      ref={scrollRef}
+    >
+      <LandingPageSection headerText={'LightningVend'} bigHeader>
         <div style={subtitleStyles}>
           <Typography variant='subtitle1'>
-            The Bitcoin Lightning Network solution for vending machines
+            The Bitcoin solution for vending machines
           </Typography>
         </div>
         <Button variant='contained' color='primary' onClick={() => navigate(adminPagePath)}>
           Get Started
         </Button>
+        <IconButton
+          style={{
+            marginTop: theme.spacing(4)
+          }}
+          color={'primary'}
+          onClick={() => scrollRef.current?.scrollBy({
+            top: window.innerHeight / 10,
+            behavior: 'smooth'
+          })}
+        >
+          <ArrowDownwardIcon/>
+        </IconButton>
       </LandingPageSection>
-      <LandingPageSection>
-        <div style={titleStyles}>
-          <Typography variant='h4'>
-            Features
-          </Typography>
-        </div>
-        <div style={featureItemStyles}>
-          <BoltIcon style={featureIconStyles}/>
-          <Typography variant='subtitle1'>
-            Fast and interoperable payments with Bitcoin Lightning Network
-          </Typography>
-        </div>
-        <div style={featureItemStyles}>
-          <TuneIcon style={featureIconStyles}/>
-          <Typography variant='subtitle1'>
-            Flexible vending machine integration
-          </Typography>
-        </div>
-        <div style={featureItemStyles}>
-          <DashboardIcon style={featureIconStyles}/>
-          <Typography variant='subtitle1'>
-            Comprehensive device and inventory management dashboard
-          </Typography>
-        </div>
+      <LandingPageSection headerText={'Features'}>
+        <LandingPageText icon={BoltIcon} verticalSpacing={0}>
+          Fast and interoperable payments with Bitcoin Lightning Network
+        </LandingPageText>
+        <LandingPageText icon={TuneIcon}>
+          Flexible vending machine integration
+        </LandingPageText>
+        <LandingPageText icon={DashboardIcon}>
+          Comprehensive device and inventory management dashboard (coming soon)
+        </LandingPageText>
+        <Button
+          style={{
+            marginTop: theme.spacing(4)
+          }}
+          variant='contained'
+          color='primary'
+          onClick={() => navigate(learnMorePagePath)}
+        >
+          Learn More
+        </Button>
       </LandingPageSection>
-      <LandingPageSection style={{maxWidth: '400px', margin: 'auto'}}>
-        <div style={titleStyles}>
-          <Typography variant='h4'>
-            How It Works
-          </Typography>
-        </div>
-        <Typography>
-          LightningVend is a software system that allows integration of Lightning Network payments
-          into vending machines.
-        </Typography>
-        <Typography>
-          Our software is flexible and supports various form-factors and
-          device types such as hand-crank gumball machines, soda/candy vending machines, sticker
-          machines, and more.
-        </Typography>
-        <Typography>
-          Devices only need power and an internet connection and use our hosted
-          software platform to provide a clean touchscreen user interface to customers.
-        </Typography>
-        <Typography>
-          Payments are
-          send to our managed LN node for high-reliability payments. You can withdraw payments to
-          any LN-compatible wallet, or setup automatic withdrawal to your own LN node.
-        </Typography>
-      </LandingPageSection>
-      <LandingPageSection>
-        <div style={titleStyles}>
-          <Typography variant='h4'>
-            Demo
-          </Typography>
-        </div>
+      <LandingPageSection headerText={'Demo'}>
         <div style={{textAlign: ('-webkit-center' as any)}}>
           <Tweet tweetId={'1608300837083684865'} options={{style: {margin: 'auto'}}}/>
         </div>
+      </LandingPageSection>
+      <LandingPageSection headerText={'Links'}>
+        <LandingPageText verticalSpacing={0}>
+          View the source code on GitHub (it's 100% open source)
+        </LandingPageText>
+        <IconButton
+          onClick={
+            () => window.open('https://github.com/tvolk131/lightning_vend', '_blank')?.focus()
+          }
+        >
+          <GitHubIcon/>
+        </IconButton>
       </LandingPageSection>
     </Box>
   );
@@ -125,22 +106,39 @@ export const LandingPage = () => {
 
 interface LandingPageSectionProps {
   children: ReactNode | ReactNode[],
-  style?: CSSProperties
+  style?: CSSProperties,
+  headerText: string,
+  bigHeader?: boolean
 }
 
 const LandingPageSection = (props: LandingPageSectionProps) => {
   const [shouldShow, setShouldShow] = useState(false);
 
-  const [fullyVisibleRef, {isVisible: isFullyVisible}] = useTrackVisibility({threshold: 0});
+  const [fullyVisibleRef, {isVisible: isAtAllVisible}] = useTrackVisibility({threshold: 0});
   const [halfVisibleRef, {isVisible: isAtLeastHalfVisible}] = useTrackVisibility({threshold: 0.5});
 
   useEffect(() => {
-    if (shouldShow && !isFullyVisible) {
+    if (shouldShow && !isAtAllVisible) {
       setShouldShow(false);
     } else if (!shouldShow && isAtLeastHalfVisible) {
       setShouldShow(true);
     }
-  }, [isFullyVisible, isAtLeastHalfVisible]);
+  }, [isAtAllVisible, isAtLeastHalfVisible]);
+
+  const theme = useTheme();
+
+  const titleStyles: CSSProperties = {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4)
+  };
+
+  const headerNode = (
+    <div style={titleStyles}>
+      <Typography variant={props.bigHeader ? 'h3' : 'h4'}>
+        {props.headerText}
+      </Typography>
+    </div>
+  );
 
   return (
     <div style={{height: '100vh', scrollSnapAlign: 'start'}}>
@@ -149,16 +147,16 @@ const LandingPageSection = (props: LandingPageSectionProps) => {
           position: 'relative',
           top: '50%',
           transform: 'translateY(-50%)',
-          padding: '20px'
+          padding: '0 20px'
         }}
       >
         <div ref={halfVisibleRef}>
           <div style={props.style} ref={fullyVisibleRef}>
-            {[props.children].flat().map((childNode, i) => {
+            {[headerNode, props.children].flat().map((childNode, i) => {
               const styles = useSpring({
                 opacity: shouldShow ? 1 : 0,
                 x: shouldShow ? 0 : -100,
-                delay: i * 100
+                delay: i * (totalSlideAnimationTimeMs / [props.children].flat().length)
               });
 
               return (
@@ -170,6 +168,35 @@ const LandingPageSection = (props: LandingPageSectionProps) => {
           </div>
         </div>
       </div>
+    </div>
+  );
+};
+
+interface LandingPageTextProps {
+  children: string,
+  icon?: React.ElementType
+  verticalSpacing?: number
+}
+
+const LandingPageText = (props: LandingPageTextProps) => {
+  const theme = useTheme();
+
+  const rootStyles: CSSProperties = {
+    alignItems: 'center',
+    marginTop: theme.spacing(props.verticalSpacing === undefined ? 4 : props.verticalSpacing),
+    display: 'inline-flex'
+  };
+
+  const iconStyles: CSSProperties = {
+    marginRight: theme.spacing(2)
+  };
+
+  return (
+    <div style={rootStyles}>
+      {props.icon && <props.icon style={iconStyles}/>}
+      <Typography variant='subtitle1'>
+        {props.children}
+      </Typography>
     </div>
   );
 };
