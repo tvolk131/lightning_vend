e655d840d35433b1062d142eb5d66eab1ba14b6a
Tommy Volk <tvolk131@gmail.com>
2023-07-03 15:15:29 -0500

    Cleanup

--- liveace_mega_sticker_machine.ino
+++ liveace_mega_sticker_machine.ino
@@ -33,12 +33,48 @@ void setup() {
   pinMode(stepper0HomingSensorPin, INPUT);
   pinMode(stepper0PowerPin0, OUTPUT);
   pinMode(stepper0PowerPin1, OUTPUT);
+  // Stepper motor is off unless we are moving it.
+  digitalWrite(stepper0PowerPin0, LOW);
+  digitalWrite(stepper0PowerPin1, LOW);
 
   stepper1.setSpeed(motorSpeed);
   pinMode(stepper1InventorySensorPin, INPUT);
   pinMode(stepper1HomingSensorPin, INPUT);
   pinMode(stepper1PowerPin0, OUTPUT);
   pinMode(stepper1PowerPin1, OUTPUT);
+  // Stepper motor is off unless we are moving it.
+  digitalWrite(stepper1PowerPin0, LOW);
+  digitalWrite(stepper1PowerPin1, LOW);
+}
+
+void loop() {
+  if (Serial.available()) {
+    command = Serial.readStringUntil('\n');
+    command.trim();
+    if (command.equals("listCommands")) {
+      Serial.println("{\"status\": \"ok\", \"command\": \"listCommands\", \"response\": {\"undefined\": [\"stepper0\", \"stepper1\"], \"boolean\": [\"stepper0Inventory\", \"stepper1Inventory\"]}}");
+    } else if (command.equals("stepper0")) {
+      moveStepper(stepper0, stepper0HomingSensorPin, stepper0PowerPin0, stepper0PowerPin1);
+      Serial.println("{\"status\": \"ok\", \"command\": \"" + command + "\", \"response\": undefined}");
+    } else if (command.equals("stepper1")) {
+      moveStepper(stepper1, stepper1HomingSensorPin, stepper1PowerPin0, stepper1PowerPin1);
+      Serial.println("{\"status\": \"ok\", \"command\": \"" + command + "\"}, \"response\": undefined}");
+    } else if (command.equals("stepper0Inventory")) {
+      String response = "{\"status\": \"ok\", \"command\": \"" + command + "\", \"response\": ";
+      bool stepper0Inventory = digitalRead(stepper0InventorySensorPin);
+      response += stepper0Inventory ? "true" : "false";
+      response += "}";
+      Serial.println(response);
+    } else if (command.equals("stepper1Inventory")) {
+      String response = "{\"status\": \"ok\", \"command\": \"" + command + "\", \"response\": ";
+      bool stepper1Inventory = digitalRead(stepper1InventorySensorPin);
+      response += stepper1Inventory ? "true" : "false";
+      response += "}";
+      Serial.println(response);
+    } else {
+      Serial.println("{\"status\": \"error\", \"command\": \"" + command + "\", \"response\": \"unknown command: `" + command + "`\"}");
+    }
+  }
 }
 
 void moveStepper(Stepper& stepper, int homingSensorPin, int powerPin0, int powerPin1) {
@@ -66,34 +102,4 @@ void moveStepper(Stepper& stepper, int homingSensorPin, int powerPin0, int power
   // Stop sending power to both stepper motor coils.
   digitalWrite(powerPin0, LOW);
   digitalWrite(powerPin1, LOW);
-}
-
-void loop() {
-  if (Serial.available()) {
-    command = Serial.readStringUntil('\n');
-    command.trim();
-    if (command.equals("list_commands")) {
-      Serial.println("{\"status\": \"ok\", \"command\": \"list_commands\", \"response\": [\"stepper0\", \"stepper1\"]}");
-    } else if (command.equals("stepper0")) {
-      moveStepper(stepper0, stepper0HomingSensorPin, stepper0PowerPin0, stepper0PowerPin1);
-      Serial.println("{\"status\": \"ok\", \"command\": \"" + command + "\"}");
-    } else if (command.equals("stepper1")) {
-      moveStepper(stepper1, stepper1HomingSensorPin, stepper1PowerPin0, stepper1PowerPin1);
-      Serial.println("{\"status\": \"ok\", \"command\": \"" + command + "\"}");
-    } else if (command.equals("stepper0Inventory")) {
-      String response = "{\"status\": \"ok\", \"command\": \"" + command + "\", \"response\": ";
-      bool stepper0Inventory = digitalRead(stepper0InventorySensorPin);
-      response += stepper0Inventory ? "true" : "false";
-      response += "}";
-      Serial.println(response);
-    } else if (command.equals("stepper1Inventory")) {
-      String response = "{\"status\": \"ok\", \"command\": \"" + command + "\", \"response\": ";
-      bool stepper1Inventory = digitalRead(stepper1InventorySensorPin);
-      response += stepper1Inventory ? "true" : "false";
-      response += "}";
-      Serial.println(response);
-    } else {
-      Serial.println("{\"status\": \"error\", \"command\": \"" + command + "\", \"response\": \"unknown command: `" + command + "`\"}");
-    }
-  }
 }
