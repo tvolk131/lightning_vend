d1892421ed4d3a715f68c7160fb5d4941f02b39e
Tommy Volk <tvolk131@gmail.com>
2023-08-03 21:12:35 -0700

    Move installation of proto compiler to new script
    This is so we don't need to reinstall the proto compiler every time we
    rebuild during development

--- proto/build.sh
+++ proto/build.sh
@@ -1,3 +1,1 @@
-sudo apt-get update
-sudo apt-get install -y protobuf-compiler
 protoc --plugin=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=forceLong=string --ts_proto_out=. ./proto/lightning_vend/model.proto ./proto/lnd/lnrpc/lightning.proto
