3032ac407b24235b8a3ee900a6ee518b1fa480ee
Tommy Volk <tvolk131@gmail.com>
2023-08-03 21:14:38 -0700

    Proto build script clears output folder
    I ran into an issue during development where the build script wouldn't
    overwrite existing files, so this ensures that running the script will
    generate fresh code on every run

--- proto/build.sh
+++ proto/build.sh
@@ -1,4 +1,8 @@
+# Remove old proto_out folder if it exists, otherwise ignore error.
+rm -r -f proto_out
+# Create new proto_out folder (and ignore error if it already exists).
 mkdir -p ./proto_out
+# Generate proto files.
 cd proto
 protoc --plugin=../node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=forceLong=string --ts_proto_out=../proto_out ./lightning_vend/model.proto ./lnd/lnrpc/lightning.proto
 cd ..
