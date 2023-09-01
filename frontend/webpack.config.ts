b8c2fbf17453d99e9708536dbcff13e97ae6f28c
Tommy Volk <tvolk131@gmail.com>
2023-08-31 22:36:27 -0500

    Update NPM dependencies

--- frontend/webpack.config.ts
+++ frontend/webpack.config.ts
@@ -13,7 +13,7 @@ export default (
   _: any,
   {watch, mode}: {watch?: boolean, mode?: string}
 ): webpack.Configuration => {
-  const maxBundleSize = mode === 'production' ? 800000 : 4300000;
+  const maxBundleSize = mode === 'production' ? 840000 : 4400000;
 
   return {
     entry: `${SRC_DIR}/index.tsx`,
