3c7fba2a159fb7f4a4ec8777a41d611d9e827123
Tommy Volk <tvolk131@gmail.com>
2023-09-03 14:24:17 -0500

    Consolidate invoice management
    Merge all logic from `DeviceUnackedSettledInvoiceManager` into
    `InvoiceManager`

--- frontend/server/persistence/invoiceManager.ts
+++ frontend/server/persistence/invoiceManager.ts
@@ -4,9 +4,6 @@ import {
   LightningClientImpl
 } from '../../proto_out/lnd/lnrpc/lightning';
 import {DeviceName} from '../../shared/proto';
-import {
-  DeviceUnackedSettledInvoiceManager
-} from './deviceUnackedSettledInvoiceManager';
 
 export class InvoiceManager {
   // TODO - Persist this in a MongoDB collection and use
@@ -14,8 +11,20 @@ export class InvoiceManager {
   private invoicesToDeviceNames: Map<string, DeviceName> = new Map();
   private lightning: LightningClientImpl;
   private intervalRef: NodeJS.Timer | undefined;
-  private deviceUnackedSettledInvoiceManager =
-    new DeviceUnackedSettledInvoiceManager();
+
+/**
+ * Stores records of settled invoices that haven't yet been acked by the devices
+ * they were issued to. Once a device has received and acked an invoice settle
+ * event, the invoice can be deleted from this manager. This ensures that the
+ * device will never miss an invoice settle event, regardless of whether the
+ * device or server is offline at the time the invoice is settled. This is
+ * important because missing or prematurely discarding an invoice settle event
+ * would result in the device never vending a product that was paid for, without
+ * any indication of an error.
+ * TODO - This should be persisted to disk. Currently it is stored in memory, so
+ * it will be lost if the server restarts.
+ */
+  private unackedSettledInvoices: SettledDeviceInvoice[] = [];
 
   public constructor(lightning: LightningClientImpl) {
     this.lightning = lightning;
@@ -58,22 +67,51 @@ export class InvoiceManager {
     return this.invoicesToDeviceNames.get(invoice);
   }
 
+  /**
+   * Stores a reference to a settled invoice and the device it was issued to.
+   * This should be called *after* the underlying Lightning node informs us that
+   * the invoice is settled and *before* we record that the invoice settle event
+   * from the underlying Lightning node has been processed (through, for
+   * example, incrementing InvoiceSubscription.settle_index if using LND).
+   * @param deviceName The device that the invoice was issued to.
+   * @param invoicePaymentRequest The invoice that was settled.
+   */
   public addUnackedSettledInvoice(
     deviceName: DeviceName,
     invoicePaymentRequest: string
-  ) {
-    return this.deviceUnackedSettledInvoiceManager
-      .addUnackedSettledInvoice(deviceName, invoicePaymentRequest);
+  ): void {
+    this.unackedSettledInvoices.push({
+      deviceName,
+      invoicePaymentRequest
+    });
   }
 
-  public getUnackedSettledInvoicesForDevice(deviceName: DeviceName) {
-    return this.deviceUnackedSettledInvoiceManager
-      .getUnackedSettledInvoicesForDevice(deviceName);
+  /**
+   * Gets all unacked settled invoices for a specified device.
+   * @param deviceName The device to get invoices for.
+   * @returns All unacked settled invoices for the specified device.
+   */
+  public getUnackedSettledInvoicesForDevice(deviceName: DeviceName): string[] {
+    return this.unackedSettledInvoices
+      .filter((unackedSettledInvoice) =>
+        unackedSettledInvoice.deviceName.equals(deviceName))
+      .map((unackedSettledInvoice) =>
+        unackedSettledInvoice.invoicePaymentRequest);
   }
 
-  public ackAndDeleteSettledInvoice(invoicePaymentRequest: string) {
-    return this.deviceUnackedSettledInvoiceManager
-      .ackAndDeleteSettledInvoice(invoicePaymentRequest);
+  /**
+   * Deletes a reference to a settled invoice. This should be called after the
+   * device has received and acked the invoice settle event.
+   * @param invoicePaymentRequest The settled invoice that was acked.
+   * @returns Whether the event was deleted.
+   */
+  public ackAndDeleteSettledInvoice(invoicePaymentRequest: string): boolean {
+    const currentEventCount = this.unackedSettledInvoices.length;
+    this.unackedSettledInvoices = this.unackedSettledInvoices.filter(
+      (unackedSettledInvoice) =>
+        unackedSettledInvoice.invoicePaymentRequest !== invoicePaymentRequest
+    );
+    return this.unackedSettledInvoices.length < currentEventCount;
   }
 
   public stop() {
@@ -91,4 +129,9 @@ export class InvoiceManager {
 
     return secondsRemainingToExpiry < 0;
   }
+}
+
+interface SettledDeviceInvoice {
+  deviceName: DeviceName;
+  invoicePaymentRequest: string;
 }
