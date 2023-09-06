8484d72d1421a289c97567bd12dcd9a31ae5a798
Tommy Volk <tvolk131@gmail.com>
2023-09-06 13:27:00 -0700

    Unfinished draft of wallet proto service

--- frontend/build_ts_proto.sh
+++ frontend/build_ts_proto.sh
@@ -4,5 +4,15 @@ rm -r -f proto_out
 mkdir -p ./proto_out
 # Generate proto files.
 cd ../proto
-protoc --plugin=../frontend/node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=forceLong=long --ts_proto_opt=useMapType=true --ts_proto_out=../frontend/proto_out ./google/api/field_behavior.proto ./lightning_vend/device_service.proto ./lightning_vend/model.proto ./lightning_vend/user_service.proto ./lnd/lnrpc/lightning.proto google/protobuf/any.proto
+protoc \
+  --plugin=../frontend/node_modules/.bin/protoc-gen-ts_proto \
+  --ts_proto_opt=forceLong=long \
+  --ts_proto_opt=useMapType=true \
+  --ts_proto_out=../frontend/proto_out \
+  ./google/api/field_behavior.proto \
+  ./lightning_vend/device_service.proto \
+  ./lightning_vend/model.proto \
+  ./lightning_vend/user_service.proto \
+  ./lightning_vend/wallet_service.proto \
+  ./lnd/lnrpc/lightning.proto google/protobuf/any.proto
 cd ../frontend
